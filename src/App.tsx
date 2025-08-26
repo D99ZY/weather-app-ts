b4b8d80500413c18b074dbcbe57e3b30c855d9b9
Damien <dtriegaardt@outlook.com>
2025-08-26 13:56:47 +1000

    Add components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import './App.css';
+import { useState } from 'react';
+import Input from './components/Input/Input.tsx';
+import Top from './components/Top/Top.tsx';
+import Bottom from './components/Bottom/Bottom.tsx';
+import styles from './App.module.css';
 
 function App() {
-  return <>Weather App</>;
+  // State variables
+  const [city, setCity] = useState('London');
+  // const [geoData, setGeoData] = useState('');
+  // const [weatherData, setWeatherData] = useState('');
+
+  // const WEATHER_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.content}>
+        <Input city={city} />
+        <Top city={city} />
+        <Bottom city={city} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
